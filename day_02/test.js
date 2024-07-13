d5a405cf3fa9f89b8e7e2f40e79b0294e308965c
Ashutosh Kumar Singh Rajbhar <121164888+AshutoshRajbhar@users.noreply.github.com>
2024-07-13 17:51:42 +0000

    Task completed

---
+++ day_02/test.js
@@ -0,0 +1,49 @@
+//Declare a variable ussing 'var', assign it a number, and log the value to the console
+
+var a = 28;
+console.log(a);
+
+// Declare a variable using 'let', assign it a string, and log the value to the console
+
+let b = "Ashutosh";
+console.log(b);
+
+// Declare a variable using 'const', assign it a value, and log the value to the console
+
+const present = true;
+console.log(present);
+
+// Create variables of different data types (number, string,
+// boolean, object, array) and log each variable using the 'typeof' operator.
+
+let fullName = "Ashutosh Kumar Singh Rajbhar";
+let age = 30;
+let isCoder = true;
+console.log(typeof fullName, age, isCoder)
+
+// Array
+
+let arr = [1,3,34,32,33];
+console.log (typeof arr);
+
+
+
+// object
+ let person = {
+    firstName : "Ashutosh",
+    lastName : "rajbhar",
+    age: 30
+ };
+console.log(person)
+console.table(person)
+
+// Reassigning Variables
+let alpha;
+console.log(alpha)
+alpha = "Ashutosh";
+console.log (alpha);
+
+
+// Understanding
+let const;
+const = 3.14;
