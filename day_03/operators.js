05b20f2fc7855506a47d48729549d02d30a9ef08
Ashutosh Kumar Singh Rajbhar <121164888+AshutoshRajbhar@users.noreply.github.com>
2024-07-14 07:03:19 +0000

    Day 3 operators

---
+++ day_03/operators.js
@@ -0,0 +1,35 @@
+//Day_02: Oprators
+// Arithmetic Operations
+
+
+// Task 1
+let a, b, sum;
+a = 23;
+b = 14;
+sum = a+b;
+console.log(sum);
+
+
+
+// Task 02
+let sub = a-b;
+console.log(sub)
+
+// to check knowledge
+let subs = b-a;
+console.log(subs)
+
+// TAsk 3
+let multiply = a*b;
+console.log(multiply)
+
+
+// task 4
+let division = a/b;
+console.log(division)
+
+
+// Activity 2: assingment Operators
+
+
+
